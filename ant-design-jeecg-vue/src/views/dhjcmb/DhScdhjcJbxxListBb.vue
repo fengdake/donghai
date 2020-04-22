198a0eceb027c9ebefbc632fad9b440efe44b856
fengdake <2834577439@qq.com>
2020-04-22 17:31:49 +0800

    修改

---
+++ ant-design-jeecg-vue/src/views/dhjcmb/DhScdhjcJbxxListBb.vue
@@ -0,0 +1,223 @@
+<template>
+  <a-card :bordered="false">
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline">
+        <a-row :gutter="24">
+          <a-col :md="6" :sm="8">
+            <a-form-item label="客户名称">
+              <a-input placeholder="请输入客户名称" v-model="queryParam.khmc"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="8">
+            <a-form-item label="证件号码">
+              <a-input placeholder="请输入证件号码" v-model="queryParam.zjhm"></a-input>
+            </a-form-item>
+          </a-col>
+
+          <a-col :md="6" :sm="8">
+            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+              <a-button
+                type="primary"
+                ghost
+                @click="searchReset"
+                icon="reload"
+                style="margin-left: 8px"
+              >重置</a-button>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+
+    <!-- 操作按钮区域 -->
+    <div class="table-operator">
+      <a-button
+        type="primary"
+        class="moBan"
+        icon="download"
+        @click="handleExportXls('首次贷后检查基本信息')"
+      >导出</a-button>
+      <!-- <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+        <a-button type="primary" icon="import">导入</a-button>
+      </a-upload>
+      <a-dropdown v-if="selectedRowKeys.length > 0">
+        <a-menu slot="overlay">
+          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
+        </a-menu>
+        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
+      </a-dropdown>-->
+    </div>
+
+    <!-- table区域-begin -->
+    <div>
+      <a-table
+        ref="table"
+        size="small"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        :scroll="{ x: 1700}"
+        @change="handleTableChange"
+      >
+        <template slot="taskResult" slot-scope="text" style="width: 100%">
+          <font :title="text" style=" display: inline-block;width:100px;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;line-height: 100%" >{{text}}</font>
+        </template>
+        <span slot="action" slot-scope="text, record">
+            <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+                    <a>删除</a>
+            </a-popconfirm>
+        </span>
+      </a-table>
+    </div>
+    <!-- table区域-end -->
+    <scdh ref="modalForm" @diaoyong="diaoyong" @ok="modalFormOk"></scdh>
+  </a-card>
+</template>
+
+<script>
+
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+import scdh from './modules/scdh'
+export default {
+  name: 'DhScdhjcJbxxList',
+  mixins: [JeecgListMixin],
+  components: {
+    scdh
+  },
+  data() {
+    return {
+      description: '首次贷后检查基本信息管理页面',
+      // 表头
+      columns: [
+        {
+          title: '序号',
+          dataIndex: '',
+          key: 'rowIndex',
+          width: 60,
+          align: 'center',
+          customRender: function(t, r, index) {
+            return parseInt(index) + 1
+          }
+        },
+        {
+          title: '客户名称',
+          align: 'center',
+          dataIndex: 'khmc'
+        },
+        {
+          title: '证件号码',
+          align: 'center',
+          dataIndex: 'zjhm'
+        },
+        {
+          title: '管户经理',
+          align: 'center',
+          dataIndex: 'ghjlmc'
+        },
+        {
+          title: '机构名称',
+          align: 'center',
+          dataIndex: 'jgmc'
+        },
+        {
+          title: '贷款金额',
+          align: 'center',
+          dataIndex: 'dkje'
+        },
+        {
+          title: '贷款余额',
+          align: 'center',
+          dataIndex: 'dkye'
+        },
+        {
+          title: '起始日期',
+          align: 'center',
+          dataIndex: 'qsrq'
+        },
+        {
+          title: '担保方式',
+          align: 'center',
+          dataIndex: 'dbfs'
+        },
+        {
+          title: '贷款用途',
+          align: 'center',
+          dataIndex: 'dkyt'
+        },
+        {
+          title: '支付方式',
+          align: 'center',
+          dataIndex: 'zffs'
+        },
+        {
+          title: '状态',
+          align: 'center',
+          dataIndex: 'zt'
+        },
+        {
+          title: '审批人',
+          align: 'center',
+          dataIndex: 'spr'
+        },
+        {
+          title: '审批内容',
+          align: 'center',
+          scopedSlots: { customRender: 'taskResult' },
+          dataIndex: 'spnr'
+        },
+         {
+          title: '操作',
+          dataIndex: 'action',
+          align: "center",
+          scopedSlots: {customRender: 'action'},
+        }
+      ],
+      url: {
+        list: '/dhjcmb/dhScdhjcJbxx/list',
+        delete: '/dhjcmb/dhScdhjcJbxx/delete',
+        exportXlsUrl: 'dhjcmb/dhScdhjcJbxx/export',
+      }
+    }
+  },
+  computed: {
+    importExcelUrl: function() {
+      return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`
+    }
+  },
+  methods: {
+    // 返回调用刷新
+    diaoyong() {
+      this.loadData()
+    },
+    //填报
+     tianBao(){
+       if (this.selectedRowKeys.length == 0) {
+        this.$notification.warn({
+          message: '提示',
+          description: `请选择填报的数据`,
+          duration: 3
+        })
+      } else {
+        //  this.$refs.modalForm.dakai()
+        this.$refs.modalForm.dakai(this.selectionRows[0])
+      }
+     }
+  }
+}
+</script>
+<style scoped>
+@import '~@assets/less/common.less';
+.moBan {
+  background: #17c295;
+  border: 1px solid #17c295;
+}
+.moBan:hover {
+  background: #3ad0a8;
+  border: 1px solid #17c295;
+}
+</style>
